ba5a8e9f7741384dc531cce0b3c8995c33b57a22
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-02-25 11:39:37 +0100

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -21,16 +21,16 @@ else
     echo "Already downloaded GWAS Catalog"
 fi
 
-# Download the GWAS DB data, if it doesn't already exist
-if [ ! -f gwasdb_20150819_snp_trait.gz ]; then
-    wget ftp://147.8.193.36/GWASdb/gwasdb_20150819_snp_trait.gz
-else
-    echo "Already downloaded GWAS DB"
-fi
-
 # Download the PHEWAS Catalog data, if it doesn't already exist
 if [ ! -f phewas-catalog.csv.zip ]; then
     wget https://phewascatalog.org/files/phewas-catalog.csv.zip
 else
     echo "Already downloaded PHEWAS Catalog"
+fi
+
+# Download the GWAS DB data, if it doesn't already exist
+if [ ! -f gwasdb_20150819_snp_trait.gz ]; then
+    wget ftp://147.8.193.36/GWASdb/gwasdb_20150819_snp_trait.gz
+else
+    echo "Already downloaded GWAS DB"
 fi
