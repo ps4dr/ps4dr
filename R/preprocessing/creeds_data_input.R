6eed77f5a14de613d22d79ef024c5d1fd3ae7062
Asif Emon <asifemon@gmail.com>
2020-02-17 09:12:16 +0100

    update...

--- R/preprocessing/creeds_data_input.R
+++ R/preprocessing/creeds_data_input.R
@@ -23,13 +23,15 @@ sprintf("Using results folder at %s", resultsFolder)
 dataFolder = file.path(resultsFolder)
 #####################################################################
 
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-load("/home/memon/projects/ps4dr/ps4dr/data/geneID_v97.RData")
+
+load(file.path(dataFolder,"geneID_v97.RData"))
 gene_id$ENTREZ = gsub("^$", NA, gene_id$ENTREZ)
 gene_id = gene_id[which(! is.na(gene_id$ENTREZ)),]
 
-creeds_melanoma = read.csv("/home/memon/projects/ps4dr/ps4dr/data/creeds/melanoma_disease_data.tsv",sep = "\t")
+#~~~~~~~~~~~: CREEDS Disease data: Melanoma :~~~~~~~~~~~~~#
+
+creeds_melanoma = fread(file.path(dataFolder, "creeds_input/melanoma_disease_data.tsv",sep = "\t"))
 creeds_melanoma = creeds_melanoma[,c(2,4,5)]
 names(creeds_melanoma) = c("efo.id","HGNC","lfc")
 creeds_melanoma$efo.id = "EFO_0000756"
@@ -38,20 +40,21 @@ lfc_comb = merge(lfc_comb, gene_id, by = "HGNC")
 lfc_comb = lfc_comb[! duplicated(lfc_comb[, c('efo.id', 'ENTREZ')]),]
 lfc_comb = lfc_comb[,c(5,2,3,4,1)]
 names(lfc_comb)
-save(lfc_comb, file = "/home/memon/projects/ps4dr/results/data/CREEDS/lfc_disease_creeds.RData")
+save(lfc_comb, file = file.path(dataFolder,"results/CREEDs/lfc_disease_creeds.RData"))
 
-#######################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-#~~~~~~~~~~~: CREEDS drug data: Bortezomib :~~~~~~~~~~~~~#
-creeds_bortezomib = fread("/home/memon/projects/ps4dr/ps4dr/data/creeds/bortezomib_drug_data.tsv",sep = "\t")
+#~~~~~~~~~~~: CREEDS Drug data: Bortezomib :~~~~~~~~~~~~~#
+
+creeds_bortezomib = fread(file.path(dataFolder, "creeds_input/bortezomib_drug_data.tsv",sep = "\t"))
 creeds_bortezomib = creeds_bortezomib[,c(2,4,5)]
 names(creeds_bortezomib) = c("chembl.id","HGNC","lfc")
 creeds_bortezomib$chembl.id = "CHEMBL325041"
 creeds_bortezomib = merge(creeds_bortezomib,gene_id,by="HGNC")
 creeds_bortezomib = creeds_bortezomib[,c(2,5,3)]
 L1000 = creeds_bortezomib
-save(L1000, file = "/home/memon/projects/ps4dr/results/data/CREEDS/lfc_drug_creeds.RData")
+save(L1000, file = file.path(dataFolder,"results/CREEDs/lfc_drug_creeds.RData"))
 
-save(spia_kegg_creeds_melanoma, spia_kegg_drug_creeds, file = "/home/memon/projects/ps4dr/results/data/CREEDS/spia_kegg_creeds.RData")
-save(drug_path,dis_path,drug_dis_path,drug_correlation,file = "/home/memon/projects/ps4dr/results/data/CREEDS/drugCorrelation_result_creeds.RData")
+save(spia_kegg_creeds_melanoma, spia_kegg_drug_creeds, file = file.path(dataFolder, "results/CREEDs/spia_kegg_creeds.RData"))
+save(drug_path,dis_path,drug_dis_path,drug_correlation, file = file.path(dataFolder, "results/CREEDs/drugCorrelation_result_creeds.RData"))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
---
+++ data/results/CREEDs/README.rst
@@ -0,0 +1,7 @@
+CREEDS Dataset
+==============
+The `Jupyter notebook <https://github.com/ps4dr/results/blob/master/data/CREEDS/creeds_loader.ipynb>`_ in this directory enables you to identify experiments in the CREEDS dataset *et al* (2020) in [1]_.
+Due to the large availability in type of experiments in the dataset, the notebook enables users to type a query disease/drug and conduct a search on the dataset to retrieve the best matches for the given query based on the experiment metadata.
+
+.. [1] `Wang, Zichen, et al. (2016) Extraction and analysis of signatures from the Gene Expression Omnibus by the crowd
+ <https://www.nature.com/articles/ncomms12846>`_. *Nature communications*, 7.1 : 1-11.
