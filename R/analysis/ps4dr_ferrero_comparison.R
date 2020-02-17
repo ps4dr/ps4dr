f14fecf7896640a3e63409090cf986051f39557d
Asif Emon <asifemon@gmail.com>
2020-02-17 08:49:00 +0100

    update...

--- R/analysis/ps4dr_ferrero_comparison.R
+++ R/analysis/ps4dr_ferrero_comparison.R
@@ -1,8 +1,4 @@
 #' additional script : PS4DR and Ferrero workflow comparison
-#' summary:
-#' This script calculates correation and dissimilarity scores for all drug-disease Gene pairs
-#' This demonstrates how the results will differ if we use anti-correlation scores of Genes instead of disrupted pathways
-
 
 suppressWarnings(suppressMessages(library(data.table)))
 suppressWarnings(suppressMessages(library(tidyr)))
@@ -31,7 +27,7 @@ dataFolder = file.path(resultsFolder)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-efo = fread("/home/memon/projects/ps4dr/ps4dr/data/EFO.csv")
+efo = fread(file.path(dataFolder, "EFO.csv"))
 efo = efo[, c(1, 2, 3)] # filter out unnecessary columns
 efo$`Class ID` = gsub(".*efo/", "", efo$`Class ID`) # replace urls to get only EFO IDs
 efo = efo[efo$`Class ID` %like% "EFO",] # filter out other disease IDs other than EFO
@@ -50,14 +46,10 @@ names(efo) = c("efo.id","Disease")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # load PS4DR results
-load("/home/memon/projects/ps4dr/ps4dr/data/results/drugCorrelation_result.RData")
+load(file.path(dataFolder,"results/drugCorrelation_result.RData"))
 
 ps4dr_shortlist = do.call(rbind, drug_shortlist)
 ps4dr_shortlist$Disease = toTitleCase(ps4dr_shortlist$Disease)
-
-# ps4dr_allNegative = do.call(rbind, drug_correlation)
-# ps4dr_allNegative = ps4dr_allNegative[Correlation.Score < 0 ]
-# ps4dr_allNegative$Disease = toTitleCase(ps4dr_allNegative$Disease)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # map disease names to efo.id
@@ -67,18 +59,11 @@ tmp[2,2] = "EFO_0003869"
 ps4dr_shortlist = merge(ps4dr_shortlist,tmp,by ="Disease")
 #ps4dr_shortlist = ps4dr_shortlist[,c(8,2,3,4,5,6,7)]
 
-# tmp = unique(ps4dr_allNegative[,c(2)])
-# tmp = merge(tmp,efo,by="Disease")
-# tmp[6,2] = "EFO_0003869"
-# tmp = unique(tmp)
-# ps4dr_allNegative = merge(ps4dr_allNegative,tmp,by ="Disease")
-# ps4dr_allNegative = ps4dr_allNegative[,c(8,2,3,4,5,6,7)]
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # load Ferrero results
-fer_true = fread("/home/memon/projects/ps4dr/ps4dr/data/13040_2018_171_MOESM2_ESM.csv")
-fer_false = fread("/home/memon/projects/ps4dr/ps4dr/data/13040_2018_171_MOESM3_ESM.csv")
+load(file.path(dataFolder,"13040_2018_171_MOESM2_ESM.csv"))
+load(file.path(dataFolder,"13040_2018_171_MOESM3_ESM.csv"))
 ferrero_all = rbind(fer_true,fer_false)
 ferrero_all$efo.term = toTitleCase(ferrero_all$efo.term)
 
@@ -89,10 +74,6 @@ comparison_shortlist = merge(ps4dr_shortlist,ferrero_all, by.x=c("efo.id","Drug"
 length(unique(comparison_shortlist$efo.id))
 comparison_shortlist_list = split(comparison_shortlist, comparison_shortlist$efo.id)
 
-# comparison_allNegative = merge(ps4dr_allNegative,ferrero_all,  by.x=c("efo.id","Drug"),by.y=c("efo.id","chembl.name"))
-# length(unique(comparison_allNegative$efo.id))
-# comparison_allNegative = split(comparison_allNegative, comparison_allNegative$efo.term)
-
 ps4dr_shortlist = split(ps4dr_shortlist,ps4dr_shortlist$Disease)
 
 comparison_table <- data.frame(matrix(ncol = 4, nrow = 12))
@@ -100,7 +81,6 @@ names(comparison_table) = c("Disease","# PS4DR_Drug","# match_with_Ferrero","PS4
 
 for (i in 1 : length(comparison_shortlist_list)) {
   for (j in 1 : length(ps4dr_shortlist)) {  
-    #if (names(comparison_shortlist_list)[[i]] == names(ps4dr_shortlist)[[j]]) {
     if (names(comparison_shortlist_list)[[i]] == ps4dr_shortlist[[j]][["efo.id"]][[1]]) {
       comparison_table[[1]][[i]] = names(ps4dr_shortlist)[[j]]
       comparison_table[[2]][[i]] = nrow(ps4dr_shortlist[[j]])
@@ -110,7 +90,7 @@ for (i in 1 : length(comparison_shortlist_list)) {
   }
 }
 
-save(comparison_shortlist,file = "/home/memon/projects/ps4dr/ps4dr/data/results/drug_comparison_ps4dr-ferrero.RData")
-write.csv(comparison_table,file = "/home/memon/projects/ps4dr/ps4dr/data/results/drug_comparison_table_ps4dr-ferrero.csv")
+save(comparison_shortlist, file = file.path(dataFolder,"results/drug_comparison_ps4dr-ferrero.RData"))
+write.csv(comparison_table, file = file.path(dataFolder,"results/drug_comparison_table_ps4dr-ferrero.csv"))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
